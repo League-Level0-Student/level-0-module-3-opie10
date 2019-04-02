062a07713cab8e8447c8bcaada7b3a8a00ec284e
Luki <>
2019-04-01 18:25:12 -0700

    week 11
    :sunglasses: super fun!!!!!!!! done dot race, had help on car race, started module 3 done birthday reminder

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -11,21 +11,33 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "	June 20th";
+		String dadsBirthday = "June 10th";
+		String myBirthday = "June 15th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
-		// 3. Print out what the user typed
+String b = JOptionPane.showInputDialog("Who's birthday do you want to know?");
 		
 		// 4. if user asked for "mom"
 			//print mom's birthday
+if (b.equals ("mom")) { 
+	JOptionPane.showMessageDialog(null, momsBirthday);
+	}
 		// 5. if user asked for "dad"
 			// print dad's birthday
+else if (b.equals ("dad")) {
+	JOptionPane.showMessageDialog(null, dadsBirthday);
+	}
 		// 6. if user asked for your name
 			// print myBirthday
+ else if (b.equals ("you")) {
+	 JOptionPane.showMessageDialog(null, myBirthday);
+	
+	 }
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+ else { 
+	 JOptionPane.showMessageDialog(null, "Sorry I do not know who you are talking about, you have come to the wrong house\n *slams door in your face*");
+ }
 	} 
 }
+
