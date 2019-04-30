354652c94aa122fa31a709e85e50b565b0586a93
Luki <>
2019-04-29 18:29:19 -0700

    :smile: week 14
    fun, done moudle3 working on processing

--- src/extra/BananaQuiz.java
+++ src/extra/BananaQuiz.java
@@ -13,16 +13,36 @@ public class BananaQuiz
 	String a	=JOptionPane.showInputDialog("Do you like bananas?");
 		//2. if they say no, 
 			if (a.equals ("No")) {
-				
-			}//tell them they are crazy 
+				JOptionPane.showMessageDialog(null, "You are CRAZY!!!!!!!!!!!!");
+			return;
+			}
+			else if(a.equals ("no")) {
+			
+				JOptionPane.showMessageDialog(null, "You are CRAZY!!!!!!!!!!!!");
+				return;
+			}
+			//tell them they are crazy 
 			//and end quiz
 		//3. if they say yes
+			else if (a.equals("Yes")) {
+				String h	=JOptionPane.showInputDialog("What is your favorite hobby");
+			JOptionPane.showMessageDialog(null, h + " is much better with bananas!");
+			return;
+			}
+			else if (a.equals("yes")) {
+				String h	=JOptionPane.showInputDialog("What is your favorite hobby");
+			JOptionPane.showMessageDialog(null, h + " is much better with bananas!");
+			return;
+			}
 		//	ask them what is their favorite hobby
 		//	show a pop up that says "<your hobby> is much better with bananas!"
 
 		//4. OPTIONAL: if they say something other than “yes�? or “no�?
 		//	show a pop up that says “You are bananas!�?
-	
+			else {
+				JOptionPane.showMessageDialog(null, "You are bananas!");
+			return;
+			}
 	}
 
 }
